9a58d1d7b2f8f7b1e44685cdb1ad356b9c57d241
Gustavo Felizardo <163663554+Scott1Mjc@users.noreply.github.com>
2026-04-06 23:49:50 -0300

    renomeando o aplicativo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,14 @@ android {
             )
         }
     }
+    
+    applicationVariants.all {
+        val variant = this
+        variant.outputs.all {
+            val output = this as com.android.build.gradle.internal.api.BaseVariantOutputImpl
+            output.outputFileName = "AmigosDaQuinta.apk"
+        }
+    }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
